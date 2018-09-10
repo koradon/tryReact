2d80a6cb339ba1ba38b1cc68859e51427184bae9
Michał Sipa <michal.sipa@mirumee.com>
2018-09-10 07:07:01 +0200

    Split PostList and PostDetail

---
+++ try-react/src/posts/PostDetail.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class PostDetail extends Component {
+    constructor(props){
+        super(props)
+        this.titleWasClicked = this.titleWasClicked.bind(this)
+    }
+
+    titleWasClicked(event){
+        event.preventDefault()
+    }
+
+    render() {
+        const {post} = this.props
+        return (
+            <div>
+                <h1 onClick={this.titleWasClicked}>{post.title}</h1>
+                <p>{post.content}</p>
+            </div>
+        );
+    }
+}
+
+export default PostDetail;
